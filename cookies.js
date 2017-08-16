c0b1c4b51f2aa25bd956959ddff367777704bfb1
Piotr <p.baranski@yahoo.pl>
2017-08-16 20:29:31 +0200

    Remove social media section' background. Change cookie info. Add temporarily map. Set up fixed height and width of authors' images. Add Piotr Barański description.

--- cookies.js
+++ cookies.js
@@ -2,7 +2,7 @@ function showCookieInfo() {
     if (!localStorage.getItem('npnd-cookie-info')) {
         var cookieInfo = document.createElement('div');
         cookieInfo.setAttribute('id', 'cookieInfo');
-        cookieInfo.innerHTML = "<p>To jest komunikat o ciasteczkach</p><span id='closeCookieInfo'>&times;</span>";
+        cookieInfo.innerHTML = "<p>Nasza strona internetowa używa plików cookies (tzw. ciasteczka) w celach statystycznych, reklamowych oraz funkcjonalnych. Dzięki nim możemy indywidualnie dostosować stronę do Twoich potrzeb. Każdy może zaakceptować pliki cookies albo ma możliwość wyłączenia ich w przeglądarce, dzięki czemu nie będą zbierane żadne informacje.</p><span id='closeCookieInfo'>&times;</span>";
         document.body.appendChild(cookieInfo);
         document.getElementById('closeCookieInfo').addEventListener('click', function () {
             document.body.removeChild(document.getElementById('cookieInfo'));
