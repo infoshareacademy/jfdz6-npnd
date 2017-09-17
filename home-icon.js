2515603583ee2bea8a38a39e330f475655ca0dfa
Piotr Baranski <p.baranski@yahoo.pl>
2017-09-17 12:46:15 +0200

    Make menu responsive. It will now shrink after scrolling down.

--- home-icon.js
+++ home-icon.js
@@ -5,7 +5,11 @@ window.onscroll = function () {
 function topButtonFunction() {
     if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         $('#topBtn').addClass('active');
+        $('.navbar').addClass('shrink');
+        $('.img-logo').addClass('shrink');
     } else {
         $('#topBtn').removeClass('active');
+        $('.navbar').removeClass('shrink');
+        $('.img-logo').removeClass('shrink');
     }
 }
