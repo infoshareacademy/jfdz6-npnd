66a18ac36669a019d3e851e3d80416d013fa075a
Piotr Baranski <p.baranski@yahoo.pl>
2017-09-17 13:31:17 +0200

    Re-format code.

--- check-scroll.js
+++ check-scroll.js
@@ -14,9 +14,9 @@ function topButtonFunction() {
     }
 }
 
-$('a').click(function(){
+$('a').click(function () {
     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
+        scrollTop: $($(this).attr('href')).offset().top
     }, 500);
     return false;
 });
