1b8e7f3da233c0d13dde38ad38be1632eece2e5e
piotrbaranski <p.baranski@yahoo.pl>
2017-10-10 18:17:58 +0200

    Add coin-cell. Coins are randomly falling on the game board.

--- game.js
+++ game.js
@@ -2,11 +2,20 @@ var $app = $('#app'); // Find element with id = 'app'
 var $table = helpers.createTable(20, 20);
 var $lastRowCells = $('tr:last td', $table);
 var $startPlayerPosition = $lastRowCells.eq(parseInt($lastRowCells.length / 2));
+var $firstRowCells = $('tr:first td', $table);
+var $startCoinPosition = $firstRowCells.eq(Math.floor(Math.random() * parseInt($firstRowCells.length)));
 
 $('td', $table).addClass('cell');
 $(function () {
     $startPlayerPosition.addClass('player-cell');
 });
+
+setInterval(function () {
+    $(function () {
+        $startCoinPosition.addClass('coin-cell');
+        $startCoinPosition = $firstRowCells.eq(Math.floor(Math.random() * parseInt($firstRowCells.length)));
+    })
+}, 1000);
 
 
 $app.append($table);
@@ -35,3 +44,8 @@ $(window).on('keydown', function (event) {
     }
 
 });
+setInterval(function () {
+    $('td.coin-cell').each(function () {
+        $(this).removeClass('coin-cell').parent().next().find('td').eq($(this).index()).addClass('coin-cell')
+    })
+}, 100);
